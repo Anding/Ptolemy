629ab1a578c23cf3539bb74423bb871a918fc641
Andrew Read <andrew81244@outlook.com>
2023-01-19 05:20:41 -0300

    Successful first tests with the 10Micron at Deep Sky Chile

--- IP.f
+++ IP.f
@@ -1,5 +1,8 @@
 \ Words to use the WinSock functionality of VFX Forth
-
+include C:\MPE\VfxForth\Lib\Win32\Genio\SocketIo.fth
+CR
+initwinsock
+CR
 : toIPv4 ( x1 x2 x3 x4 -- x1.x2.x3.x4 )
 \ convert four separate numbers to a four-byte IPv4 number
 \ network order is assumed
--- test10Micron.f
+++ test10Micron.f
@@ -1,20 +1,79 @@
-\ VFX Winsock support
-include C:\MPE\VfxForth\Lib\Win32\Genio\SocketIo.fth
-CR
+\ value type holding the socket number of the 10Micron mount
+0 value MNTSOC
 
-\ helper functions
-: toIPv4 ( x1 x2 x3 x4 -- x1.x2.x3.x4 )
-\ convert four separate numbers to a four-byte IPv4 number
-\ network order is assumed
-	>R >R >R
-	256 * R> +
-	256 * R> +	
-	256 * R> +
-;
+\ buffer to hold strings received from the 10Micron mount
+256 buffer: MNTBUF
 
 \ try to connect to the 10 Micron mount at Deep Sky Chile
-initwinsock
-CR
-192 168 1 107 toIPv4 0 3490 TCPConnect
-. dup .
+: mount.connect ( -- )
+	CR
+	192 168 1 107 toIPv4 0 3490 TCPConnect
+	SOCKET_ERROR = if 
+		." Connection failed with WinSock error number "
+	else
+		." Connection succeeded on socket "
+		dup -> MNTSOC
+	then
+	dup . CR
+	drop \ better to leave the result?
+;
+
+\ check for an uninitialized socket
+: mount.checksocket ( -- ior)
+	CR
+	MNTSOC 0 = if 
+		CR." Use mount.connect first" CR -1 
+	else
+		0
+	then
+;
+	
+\ pass a command string to the mount
+: mount.tell ( c-addr u --)
+	CR
+	mount.checksocket if drop drop exit then
+	dup -rot 						( u c-addr u)
+	MNTSOC writesock		( u len 0 | u error SOCKET_ERROR)
+	SOCKET_ERROR = if ." Failed to write to the socket with error " . CR exit then
+	<> if ." Failed to write the full string to the socket" CR exit then
+;
+
+\ get a response from the mount
+: mount.ask ( -- c-addr u)
+	CR
+	mount.checksocket if MNTBUF 0 exit then
+	0 >R 5														( tries R:bytes)
+	begin
+		1- dup 0 >=
+	while
+		100 ms
+		MNTSOC pollsock									( tries len | tries SOCKET_ERROR)
+		dup SOCKET_ERROR = if 
+			drop ." Failed to poll the socket " CR
+		else
+			0= if
+				." 0 bytes available at the socket" CR
+			else
+				MNTBUF 256 MNTSOC readsock 	( tries len 0 | tries error SOCKET_ERROR)
+				SOCKET_ERROR = if						( tries ior)
+					." Failed to read the socket with error " . CR
+				else												( tries len)
+					R> drop >R drop 0					( 0 R:bytes)
+				then												( tries R:bytes)
+			then
+		then
+	repeat
+	drop MNTBUF R>
+;
+	
+\ obtain mount status
+: mount.status
+	CR
+	s" :Gstat#" mount.tell
+	mount.ask 
+	?dup 0= if ." No response from the mount" CR drop exit then
+	type
+;
+	
+		
 
