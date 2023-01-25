89a9eef83ae3ff0e9f5733f412d91014a4f04a9c
Andrew Read <andrew81244@outlook.com>
2023-01-25 07:05:54 -0300

    live testing updates to the serial port control of the SQM

--- SQM.f
+++ SQM.f
@@ -1,6 +1,6 @@
 \ Use the serial port (COM) functionality of VFX Forth to communicate with a Unihedron Sky Quality Meter
 
-include %vfxpath%\Lib\Win32\Genio\SocketIo.fth
+include C:\MPE\VfxForth\Lib\Win32\Genio\serialbuff
 
 256 buffer: SQMBUF
 \ buffer to hold strings communicated with the SQM
@@ -9,7 +9,6 @@ serdev: SQMCOM
 \ prepare an instance of a VFX Forth generic I/O driver
 
 s" COM4: baud=115200 parity=N data=8 stop=1" ( legacy setting) 7 SQMCOM open-gio . . CR
-. . 
 \ prepare a suitable connection string
 \ open the port
 
