bcac65a398833db77b45dab9ab023e204ff262c2
Andrew Read <andrew81244@outlook.com>
2024-12-23 20:38:05 +0800

    read mount data at DSC

---
+++ dashboard/DSC_test1.f
@@ -0,0 +1,17 @@
+include %idir%\..\ForthBase\libraries\libraries.f
+NEED Forth10Micron
+
+192 168 1 107 toIPv4 value 10Micron.IP
+
+add-mount
+
+what-mount?
+
+mount_horizon
+." AZ " ~. CR
+." ALT" ~. CR
+
+mount_location
+." ELEV" . CR
+." LONG" ~. CR
+." LAT" ~. CR
