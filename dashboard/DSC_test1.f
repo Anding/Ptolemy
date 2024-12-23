c3adb1635798e18ad89bfa2cada186d2693f7805
Andrew Read <andrew81244@outlook.com>
2024-12-23 10:35:36 -0300

    testing with the mount at DSC

--- dashboard/DSC_test1.f
+++ dashboard/DSC_test1.f
@@ -2,11 +2,9 @@ include %idir%\..\..\ForthBase\libraries\libraries.f
 NEED Forth10Micron
 NEED forth-map
 
-192 168 1 107 toIPv4 value 10Micron.IP
+192 168 1 107 toIPv4 -> 10Micron.IP
 
 add-mount
-
-what-mount?
 
 map CONSTANT FITSmap
 FITSmap add-mountFITS 
