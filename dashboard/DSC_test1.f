b8524ca7dae60a58559672034c3df13e5cc06c17
Andrew Read <andrew81244@outlook.com>
2024-12-23 20:41:07 +0800

    prepare to commit DSC_test1.f to DSC

--- dashboard/DSC_test1.f
+++ dashboard/DSC_test1.f
@@ -1,5 +1,6 @@
 include %idir%\..\ForthBase\libraries\libraries.f
 NEED Forth10Micron
+NEED forth-map
 
 192 168 1 107 toIPv4 value 10Micron.IP
 
@@ -7,11 +8,6 @@ add-mount
 
 what-mount?
 
-mount_horizon
-." AZ " ~. CR
-." ALT" ~. CR
-
-mount_location
-." ELEV" . CR
-." LONG" ~. CR
-." LAT" ~. CR
+map CONSTANT FITSmap
+FITSmap add-mountFITS 
+CR FITSmap .map CR
