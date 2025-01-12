338ef878e1af0c8176e0c48e382846fbb6a67f76
Andrew Read <andrew81244@outlook.com>
2025-01-12 16:45:03 -0300

    debugging at DSC

--- dashboard/DSC_test1.f
+++ dashboard/DSC_test1.f
@@ -13,41 +13,42 @@ CR ." Set high precision mode"
 10u.HighPrecisionOn
 
 map CONSTANT DASHmap
-DASHmap add-mountDASH 
-CR DASHmap .map CR
+DASHmap add-mountDASH
+DASHmap +map				\ need to expose the map to compile the keywords in .DASHBOARD
 
-: .DASHBOARD ( map --)
-	dup +map >R
-	12 5 1 table vt.home
+: .DASHBOARD ( --)
+	0 12 1 table
 	assign vt.green to-do table.text-format
 	assign vt.blue to-do table.line-format 
 
 	table.cellWidth dup 4 * 3 + -> table.cellWidth	\ merge 4 columns
 	row |h ." Deep Sky Chile 10Micron mount telemetry"		-> table.cellWidth	\ restore column width
-	row |t s" Local time" RJ.	|t LOCALTME RJ.		|t s" Sidereal time"		|t SIDEREAL RJ.	
-	row |r 							|r 						|r 							|r
-	row |t s" Status" RJ.		|t STATUS RJ.			|t s" TRACKING" RJ.		|t TRACKING RJ.	
+	row |r 							|r 						|r 
+	row |t s" Local time" RJ.	|t LOCALTME RJ.		|t s" Sidereal time" RJ.		|t SIDEREAL RJ.	
+	row |l 							|l 						|l 							|r
+	row |t s" Status" RJ.		|t STATUS RJ.			|t s" Tracking" RJ.		|t TRACKING RJ.	
 	row |l 							|l 						|l 							|l	
 	row |t s" R.A." RJ.			|t OBJCTRA RJ.			|t s" DEC" RJ.				|t OBJCTDEC RJ.
 	row |l 							|l 						|l 							|l
 	row |t s" Alt" RJ.			|t OBJCTALT RJ.		|t s" AZ" RJ.				|t OBJCTAZ RJ.
 	row |l 							|l 						|l 							|l
-	row |t s" H.A." RJ.			|t OBJCTHA RJ.			|t s" Track end" RJ.		|t TRACKEND RJ.									|t
+	row |t s" H.A." RJ.			|t OBJCTHA RJ.			|t s" Track end" RJ.		|t TRACKEND RJ.						
 	row |l 							|l 						|l 							|l
 	row |t s" Pier side" RJ.	|t PIERSIDE RJ.		|t s" Target side" RJ.	|t TGTPSIDE RJ.	
 	row |f 							|f 						|f 							|f
 
-	R> -map
 ;
 
 : cycle-dashboard
 	begin 
-		vt.reset vt.cursor_off vt.cls 
+		vt.reset vt.cursor_off vt.cls
 	-1 while
-		key? if CR vt.cursor_on exit then
+		key? if CR vt.cursor_on vt.reset exit then
 		DASHmap add-mountDASH
-		.DASHBOARD
+		vt.home .DASHBOARD
 	repeat
 ;
 
-cycle-dashboard
+vt.reset vt.cursor_off vt.cls vt.home
+.DASHBOARD 
+CR vt.cursor_on vt.reset
